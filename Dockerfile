83419b10b8a4722bcda7edacf622c57fd6a1000f
Alp <alp.tuna.453@gmail.com>
2024-01-22 01:42:07 +0300

    Add more configurations

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:19-alpine as base
+FROM node:18-alpine as base
 WORKDIR /app
 COPY package.json /app
 COPY package-lock.json /app
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import helmet from '@fastify/helmet';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   const configService = new ConfigService();
-  await app.listen(configService.get('PORT'));
+  const swaggerEnabled = configService.get('SWAGGER_ENABLE') !== 'false';
+  if (swaggerEnabled) {
+    const config = new DocumentBuilder()
+      .setTitle('Scouthub API Admin Gateway')
+      .setDescription('Scouthub API Admin Gateway Documentation')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+  }
+
+  app.enableCors();
+  app.register(helmet, {
+    contentSecurityPolicy: false,
+  });
+  await app.listen(configService.get('PORT'), '0.0.0.0');
 }
 bootstrap();
